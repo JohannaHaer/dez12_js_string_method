6aa19c4fc6ee6914882a075d92fb5904de026cde
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-12 12:54:31 +0100

    String Method 1.4 fertig

---
+++ assets/js/main_string_1_4.js
@@ -0,0 +1,14 @@
+const A = 'Susi is going to codingschool'
+
+let myVar = A.slice(0, 4)
+let susi = document.write(myVar+" ")
+
+let myVar2 = A.slice(5, 7)
+let is = document.write(myVar2+" ")
+
+let myVar3 = A.slice(5, 17)
+let myVar4 = A.slice(-6)
+let isGoingTo = document.write(myVar3 + " " +myVar4)
+let school = document.write(myVar4)
+
+let susiIsSchool = document.write(myVar + " " + myVar2 + " " + myVar4)
