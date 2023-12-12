ef1e1a7a1392ef03ac66933116b1f9a051de13fa
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-12 12:30:40 +0100

    String Method 1.1 fertig

---
+++ assets/js/main_string_1_1.js
@@ -0,0 +1,9 @@
+let firstName = "Johanna"
+let lastName = "Hä"
+
+console.log(firstName.length);
+console.log(lastName.length);
+
+let fullName = firstName + " " + lastName
+
+console.log(fullName.length);
